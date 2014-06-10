12355b41172e6a77d691f8c289328202e0f766b4
dmm42 <diegmonti@gmail.com>
2014-06-10 21:07:33 +0200

    Add bot edits

--- edits.sh
+++ edits.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Edit totali,Edit minori,Edit di IP,Edit revertati
+# Edit totali,Edit minori,Edit IP,Edit revertati,Edit bot
 
 dati=$(date +%Y%m%d)
 
@@ -14,6 +14,9 @@ dato=$(mysql --defaults-file=replica.my.cnf -h s2.labsdb -e "SELECT COUNT(*) FRO
 dati+=",$dato"
 
 dato=$(mysql --defaults-file=replica.my.cnf -h s2.labsdb -e "SELECT COUNT(*) FROM revision WHERE rev_deleted = 1" itwiki_p | tail -1)
+dati+=",$dato"
+
+dato=$(mysql --defaults-file=replica.my.cnf -h s2.labsdb -e "SELECT COUNT(*) FROM revision WHERE rev_user IN (SELECT ug_user FROM user_groups WHERE ug_group = 'bot')" itwiki_p | tail -1)
 dati+=",$dato"
 
 echo $dati >> public_html/data/edits.csv
