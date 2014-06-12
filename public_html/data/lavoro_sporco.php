c48912303932c8d921c3d5ce77bd5fdcfa22f62e
dmm42 <diegmonti@gmail.com>
2014-06-12 18:17:45 +0200

    Update lavoro_sporco.php and fix indentation

--- public_html/data/lavoro_sporco.php
+++ public_html/data/lavoro_sporco.php
@@ -14,14 +14,44 @@ if (($handle = fopen($file, 'r')) !== FALSE) {
         }
       } else {
         if ($j > 0) {
-          $arr[$j-1][1]['data'][$i-1]['price'] = $lineArray[$j];
           $arr[$j-1][1]['data'][$i-1]['month'] = (int)substr($lineArray[0],4,2);
           $arr[$j-1][1]['data'][$i-1]['year'] = (int)substr($lineArray[0],0,4);
           $arr[$j-1][1]['data'][$i-1]['day'] = (int)substr($lineArray[0],6,2);
+		  $arr[$j-1][1]['data'][$i-1]['tot'] = $lineArray[$j];
         }
       }
     }
   $i++;
+  }
+  fclose($handle);
+}
+
+$file = 'diff.csv';
+
+if (($handle = fopen($file, 'r')) !== FALSE) {
+  fgetcsv($handle); // remove first line
+  $i = 1;
+  while ($i < 5) { // offset
+    $z = 0;
+    for ($j = 0; $j < 24; $j++) {
+      $arr[$z][1]['data'][$i-1]['add'] = (string)0;
+      $arr[$z][1]['data'][$i-1]['rem'] = (string)0;
+      $arr[$z][1]['data'][$i-1]['diff'] = (string)0;
+      $z++;
+	}
+    $i++;
+  }
+  while (($lineArray = fgetcsv($handle)) !== FALSE) {
+    $z = 0;
+    for ($j = 0; $j < count($lineArray); $j++) {
+      if ($j % 2) {
+        $arr[$z][1]['data'][$i-1]['add'] = $lineArray[$j];
+        $arr[$z][1]['data'][$i-1]['rem'] = $lineArray[$j+1];
+        $arr[$z][1]['data'][$i-1]['diff'] = (string)($lineArray[$j] - $lineArray[$j+1]);
+        $z++;
+      }
+	}
+    $i++;
   }
   fclose($handle);
 }
