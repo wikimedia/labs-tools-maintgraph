f67a231c1912b1dd29da444277b0752583736acf
dmm42 <diegmonti@gmail.com>
2014-06-13 21:49:35 +0200

    Update lavoro_sporco.php

--- public_html/data/lavoro_sporco.php
+++ public_html/data/lavoro_sporco.php
@@ -30,24 +30,14 @@ $file = 'diff.csv';
 
 if (($handle = fopen($file, 'r')) !== FALSE) {
   fgetcsv($handle); // remove first line
-  $i = 1;
-  while ($i < 5) { // offset
-    $z = 0;
-    for ($j = 0; $j < 24; $j++) {
-      $arr[$z][1]['data'][$i-1]['add'] = (string)0;
-      $arr[$z][1]['data'][$i-1]['rem'] = (string)0;
-      $arr[$z][1]['data'][$i-1]['diff'] = (string)0;
-      $z++;
-	}
-    $i++;
-  }
+  $i = 0;
   while (($lineArray = fgetcsv($handle)) !== FALSE) {
     $z = 0;
     for ($j = 0; $j < count($lineArray); $j++) {
       if ($j % 2) {
-        $arr[$z][1]['data'][$i-1]['add'] = $lineArray[$j];
-        $arr[$z][1]['data'][$i-1]['rem'] = $lineArray[$j+1];
-        $arr[$z][1]['data'][$i-1]['diff'] = (string)($lineArray[$j] - $lineArray[$j+1]);
+        $arr[$z][1]['data'][$i]['add'] = $lineArray[$j];
+        $arr[$z][1]['data'][$i]['rem'] = $lineArray[$j+1];
+        $arr[$z][1]['data'][$i]['diff'] = (string)($lineArray[$j] - $lineArray[$j+1]);
         $z++;
       }
 	}
