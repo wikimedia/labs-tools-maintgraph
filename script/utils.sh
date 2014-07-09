323d2ac9411472a307647f7958b4222a95277b9b
dmm42 <diegmonti@gmail.com>
2014-07-09 12:31:40 +0200

    Fix utils query

--- script/utils.sh
+++ script/utils.sh
@@ -10,7 +10,7 @@ dati+=",$dato"
 dato=$(mysql --defaults-file=replica.my.cnf -h s2.labsdb -e "SELECT COUNT(*) FROM page WHERE page_is_redirect = 1 AND page_namespace = 0" itwiki_p | tail -1)
 dati+=",$dato"
 
-dato=$(mysql --defaults-file=replica.my.cnf -h s2.labsdb -e "SELECT COUNT(*) FROM templatelinks WHERE tl_namespace = 10 AND (tl_title = 'Disambigua' OR tl_title = 'Sigla2' OR tl_title = 'Sigla3')" itwiki_p | tail -1)
+dato=$(mysql --defaults-file=replica.my.cnf -h s2.labsdb -e "SELECT COUNT(*) FROM page_props WHERE pp_propname = 'disambiguation'" itwiki_p | tail -1)
 dati+=",$dato"
 
 dato=$(mysql --defaults-file=replica.my.cnf -h s2.labsdb -e "SELECT COUNT(*) FROM templatelinks WHERE tl_namespace = 10 AND (tl_title = 'Interprogetto' OR tl_title = 'Ip')" itwiki_p | tail -1)
