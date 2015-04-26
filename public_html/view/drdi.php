22033377f957c573c7f4b98048b2ab72451bce64
dmm42 <diegmonti@gmail.com>
2015-04-26 20:39:41 +0200

    Using tools-static for css and js

--- public_html/view/drdi.php
+++ public_html/view/drdi.php
@@ -37,8 +37,7 @@ echo <<<END
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="//tools-static.wmflabs.org/static/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
 
     <style type="text/css">
         html {
@@ -118,8 +117,8 @@ echo <<<END
         }
     </style>
 
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/jquery.min.js"></script>
+    <script src="//tools-static.wmflabs.org/static/jquery/2.1.0/jquery.min.js"></script>
+    <script src="//tools-static.wmflabs.org/static/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
 </head>
 
