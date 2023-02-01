9f334c17c4919c8a97b83a5e8e954da6319c95c9
Incola <diegmonti@gmail.com>
2023-02-01 21:26:30 +0000

    Fix utils query

--- script/utils.sh
+++ script/utils.sh
@@ -13,10 +13,10 @@ dati+=",$dato"
 dato=$(mysql --defaults-file=replica.my.cnf -h itwiki.analytics.db.svc.eqiad.wmflabs -e "SELECT COUNT(*) FROM page_props WHERE pp_propname = 'disambiguation'" itwiki_p | tail -1)
 dati+=",$dato"
 
-dato=$(mysql --defaults-file=replica.my.cnf -h itwiki.analytics.db.svc.eqiad.wmflabs -e "SELECT COUNT(*) FROM templatelinks WHERE tl_namespace = 10 AND (tl_title = 'Interprogetto' OR tl_title = 'Ip')" itwiki_p | tail -1)
+dato=$(mysql --defaults-file=replica.my.cnf -h itwiki.analytics.db.svc.eqiad.wmflabs -e "SELECT COUNT(*) FROM templatelinks JOIN linktarget ON tl_target_id = lt_id WHERE lt_namespace = 10 AND (lt_title = 'Interprogetto' OR lt_title = 'Ip')" itwiki_p | tail -1)
 dati+=",$dato"
 
-dato=$(mysql --defaults-file=replica.my.cnf -h itwiki.analytics.db.svc.eqiad.wmflabs -e "SELECT COUNT(*) FROM templatelinks WHERE tl_namespace = 10 AND (tl_title = 'Portale' OR tl_title = 'Portali')" itwiki_p | tail -1)
+dato=$(mysql --defaults-file=replica.my.cnf -h itwiki.analytics.db.svc.eqiad.wmflabs -e "SELECT COUNT(*) FROM templatelinks JOIN linktarget ON tl_target_id = lt_id WHERE lt_namespace = 10 AND (lt_title = 'Portale' OR lt_title = 'Portali')" itwiki_p | tail -1)
 dati+=",$dato"
 
 cat public_html/data/utils.csv | grep $(date +%Y%m%d -d "yesterday") > /dev/null
